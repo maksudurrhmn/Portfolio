35d204fcf80c7f9da47e10be01cbb121a8afd4bc
maksudurrhmn <mmaksudurrahman247@gmail.com>
2025-11-16 07:30:54 +0600

    Add Initial Files

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Toggle Menu or Hamburger Menu
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+// Scrolling Animation
+let allSection = document.querySelectorAll(".animation");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+}, {});
+
+allSection.forEach((el) => observer.observe(el));
